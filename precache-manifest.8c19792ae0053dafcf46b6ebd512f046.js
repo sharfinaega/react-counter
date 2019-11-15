cb31491ea24d9bdf93d873d052bc9f11cad3b08f
sharfinaega <ega.sharfina@gmail.com>
2019-11-15 15:58:25 +0700

    Updates

---
+++ precache-manifest.8c19792ae0053dafcf46b6ebd512f046.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93a76858a5b744cfa1d0a629e544ea4e",
+    "url": "/react-counter/index.html"
+  },
+  {
+    "revision": "44dfb39a453011b3810f",
+    "url": "/react-counter/static/css/main.a24bed15.chunk.css"
+  },
+  {
+    "revision": "1be503cacff1274e0b41",
+    "url": "/react-counter/static/js/2.4d93024b.chunk.js"
+  },
+  {
+    "revision": "44dfb39a453011b3810f",
+    "url": "/react-counter/static/js/main.ee92bed4.chunk.js"
+  },
+  {
+    "revision": "9f4cbe663b856c63d285",
+    "url": "/react-counter/static/js/runtime-main.aeeb9132.js"
+  }
+]);
